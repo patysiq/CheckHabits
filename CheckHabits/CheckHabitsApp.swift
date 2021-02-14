fbd95d73d560654c37b243261aa56d9876d190a5
Patricia <patysiq@yahoo.com.br>
2021-02-14 09:05:41 -0300

    Initial Commit

---
+++ CheckHabits/CheckHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckHabitsApp.swift
+//  CheckHabits
+//
+//  Created by PATRICIA S SIQUEIRA on 14/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
